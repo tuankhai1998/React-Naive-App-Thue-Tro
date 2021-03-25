d19a75556490f354aebc4bd5982e299a13932130
LÂM TUẤN KHẢI <44898230+tuankhai1998@users.noreply.github.com>
2021-03-25 17:57:58 +0700

    update search screen

--- src/screens/Search.screen.js
+++ src/screens/Search.screen.js
@@ -1,11 +1,13 @@
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/core';
 import React, { useState } from 'react'
-import { View, Text, SafeAreaView, TouchableOpacity } from 'react-native'
-import { COLORS, FONTS, SIZES } from '../constants';
+import { View, Text, SafeAreaView, TouchableOpacity, FlatList, Image } from 'react-native'
+import { COLORS, FONTS, Images, SIZES } from '../constants';
+import { AntDesign } from '@expo/vector-icons';
 
 export default function SearchScreen() {
-    let [modalVisible, setModalVisible] = useState(false);
+    let [modalVisible, setModalVisible] = useState(false),
+        [data, setData] = useState([1, 2])
     const navigation = useNavigation();
 
     const renderHeader = () => {
@@ -76,16 +78,112 @@ export default function SearchScreen() {
             </View>
         )
     }
+
+    const renderItemSearch = ({ item }) => {
+        return (
+            <TouchableOpacity
+                style={{
+                    flexDirection: 'row',
+                    marginHorizontal: SIZES.base / 2,
+                    padding: SIZES.base,
+                    alignItems: 'center'
+                }}
+            >
+                <AntDesign name="clockcircleo" size={18} color="black" />
+                <Text
+                    style={{
+                        marginLeft: SIZES.base
+                    }}
+                >{item}</Text>
+            </TouchableOpacity>
+        )
+    }
+
+    const renderItem = ({ item }) => {
+        return (
+            <TouchableOpacity
+                style={{
+                    flexDirection: 'row',
+                    marginBottom: SIZES.base,
+                    backgroundColor: COLORS.white,
+                    borderRadius: SIZES.radius / 2
+                }}
+            >
+                <Image source={Images.DONGDA} style={{ width: SIZES.width / 4, height: SIZES.width / 4, borderRadius: SIZES.radius / 2 }} />
+                <View
+                    style={{
+                        flex: 1,
+                        marginLeft: SIZES.base,
+                        paddingHorizontal: SIZES.base
+                    }}
+                >
+                    <View
+                        style={{
+                            flexDirection: 'row',
+                            justifyContent: 'space-between'
+                        }}
+                    >
+                        <Text style={{ ...FONTS.body4, textTransform: "uppercase", fontSize: 12 }}>Tìm người thuê</Text>
+                        <Text
+                            style={{
+                                ...FONTS.body4,
+                                fontWeight: 'bold',
+                                color: COLORS.secondary
+                            }}
+                        >
+                            1.2 triệu/phòng
+        </Text>
+                    </View>
+
+                    <Text style={{ ...FONTS.h4 }} numberOfLines={2}>
+                        Phòng cho thuê ở Duy Tân quận Cầu Giấy
+        </Text>
+
+                    <Text numberOfLines={2} ellipsizeMode='middle'>3A, ngõ 82, Duy Tân, Dịch Vọng Hậu, Cầu Giấy</Text>
+                </View>
+            </TouchableOpacity>
+        )
+    }
+
     return (
         <View
             style={{
                 flex: 1,
-                backgroundColor: 'red'
+                backgroundColor: 'rgba(0,0,0,0.1)',
+
             }}
         >
 
             {renderHeader()}
-            <Text>Hello this is search screen</Text>
-        </View>
+            {
+                data ? (
+                    <View
+                        style={{
+                            padding: SIZES.base
+                        }}
+                    >
+                        <FlatList
+                            data={data}
+                            renderItem={renderItem}
+                        />
+
+                    </View>
+                ) : (
+                    <View
+                        style={{
+                            padding: SIZES.base
+                        }}
+                    >
+                        <Text style={{ ...FONTS.h4 }}>Lịch sử tìm kiếm</Text>
+                        <FlatList
+                            data={[1, 2]}
+                            renderItem={renderItemSearch}
+                            keyExtractor={item => item.id}
+                        />
+
+                    </View>
+                )
+            }
+        </View >
     )
 }
