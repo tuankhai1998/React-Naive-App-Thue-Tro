855766ae78bea91c9907c43e138509735093cca8
LÂM TUẤN KHẢI <44898230+tuankhai1998@users.noreply.github.com>
2021-04-20 13:19:17 +0700

    any

--- App.js
+++ App.js
@@ -23,3 +23,24 @@ export default function App() {
   )
 }
 
+/**
+ * get location current
+ *  const getLocationAsync = async () => {
+ *  let { status } = await Location.requestPermissionsAsync();
+
+        if (status !== 'granted') {
+            setErrorMsg('Permission to access location was denied');
+        }
+
+        let location = await Location.getCurrentPositionAsync({});
+
+        // React native
+1. Header slide
+https://medium.com/@KPS250/creating-image-slider-with-flatlist-in-react-native-1815d3793d99
+2. Opent navigation directly
+https://supersami.medium.com/react-native-integrating-with-google-maps-to-maps-or-navigation-android-fbc5cb31bc59
+or
+https://www.npmjs.com/package/react-native-google-maps-directions
+3. https://github.com/jsdf/react-native-htmlview
+ */
+
